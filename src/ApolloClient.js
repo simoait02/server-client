acbb04db5bad38296228c86ad232e3e2a537ccfb
simoait02 <simoaithssaine09@gmail.com>
2025-03-13 15:37:32 +0000

    initial commit

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:8080/graphql', // Your Spring Boot endpoint
+    uri: 'http://localhost:8080/graphql',
     cache: new InMemoryCache(),
     headers: {
         authorization: localStorage.getItem('authToken') || ''
